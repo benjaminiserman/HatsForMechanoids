288168ab6e9c8dfb2a072679a90a3be1ea274ac0
winggar <winggar1228@gmail.com>
2025-08-08 01:56:44 -0700

    they have hats! still need to position them better tho

--- Source/HatsForMechanoids/HatWearerDef.cs
+++ Source/HatsForMechanoids/HatWearerDef.cs
@@ -11,11 +11,11 @@ namespace HatsForMechanoids
         public OffsetsForDirection east = new OffsetsForDirection();
         public OffsetsForDirection south = new OffsetsForDirection();
         public OffsetsForDirection west = new OffsetsForDirection();
-    }
 
-    public class OffsetsForDirection
-    {
-        public float xOffset = 0f;
-        public float zOffset = 0f;
+        public class OffsetsForDirection
+        {
+            public float xOffset = 0f;
+            public float zOffset = 0f;
+        }
     }
 }
